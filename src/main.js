66e67a7fef63bc8b8ba32f5061f697e597fd12b1
shrestharohit <rohitshr98@gmail.com>
2020-01-20 15:54:01 +0545

    vuetify plugin add

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import axios from 'axios'
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 Vue.prototype.$axios = axios
@@ -11,5 +12,6 @@ Vue.prototype.$axios = axios
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
