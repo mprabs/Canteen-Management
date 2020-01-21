fbdedb384a04fe56fc217df0745d0534e0669fd4
shrestharohit <rohitshr98@gmail.com>
2020-01-21 15:47:35 +0545

    vuetify added for designing

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,18 +1,69 @@
 <template>
-<div class="Login">
-<img src="../assets/aayulogic.png" style="width:30%; height:30%;"><br><br><br><br>
-    <div id="container">
-    <div id="loginBox" style="font-family: calibri;">
-      <div style=" background-color: #3f70c4">
-      <h3 id="headLine" > AAYULOGIC  CANTEEN  MANAGEMENT </h3> <hr>
-      </div> <br><br>
-        <input type="text" name="username" v-model="input.username" placeholder="Username" /><br><br>
-        <input type="password" name="password" v-model="input.password" placeholder="Password" /><br><br>
-        <button type="button" v-on:click="login()" style="font-family: calibri; font-type: bold;">Login</button><br><br>
-    </div><br><br>
+  <div class="Login">
+    <!-- <span class="bg"></span> -->
+    <v-app id="inspire" >
+      <v-content>
+        <v-img
+          src="../assets/canteen.jpg"
+          gradient="to top right, rgba(100,115,201,.33), rgba(25,32,72,.7)"
+          width="100%"
+          height=100%>
+       <v-container class="fill-height" >
+           <v-card max-width="65%">
+            <v-carousel hide-delimiter-background height="450">
+              <v-carousel-item
+                src="../assets/canteen.jpg"
+                >
+              </v-carousel-item>
+            </v-carousel>
+          </v-card>
+
+          <v-spacer />
+          <v-card class="elevation-12" color="grey lighten-2" width="30%" height="350">
+              <v-toolbar color="grey lighten-2" class="blue--text text--darken-3" dark flat>
+                <!-- <v-toolbar-title>Canteen</v-toolbar-title> -->
+                <v-spacer />
+                <v-img
+                  src="../assets/aayulogic.png"
+                  height="20"
+                  width="20"
+                ></v-img>
+              </v-toolbar><hr color="blue" >
+              <v-card-text>
+                <v-form>
+                  <v-text-field
+                    type="text"
+                    v-model="input.username"
+                    label="Username"
+                  />
+                  <v-text-field
+                    type="password"
+                    v-model="input.password"
+                    label="Password"
+                  />
+                  <v-card-actions>
+                    <v-spacer /><v-btn
+                  tile
+                  color="primary"
+                  dark
+                  v-on:click="login()">Login</v-btn>
+                  </v-card-actions>
+                </v-form>
+            </v-card-text>
+            </v-card>
+       </v-container>
+       </v-img>
+       <v-footer :fixed="true" :padless="true">
+          <v-card flat tile width="100%" class="purple darken-4 text-center">
+          <v-card-text class="white--text">
+            {{ new Date().getFullYear() }} — <strong>Aayulogic</strong>
+          </v-card-text>
+          </v-card>
+      </v-footer>
+    </v-content>
+    </v-app>
     </div>
-  </div>
-</template>
+  </template>
 
 <script>
 export default {
@@ -58,41 +109,3 @@ export default {
   }
 }
 </script>
-
-<style scoped>
-#loginBox {
-  width: 450px;
-  border: 1px solid rgb(176, 199, 199);
-  background-color: rgb(255, 255, 255);
-  margin: auto;
-}
-
-#headLine {
-  color: white;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  margin-top: 0px;
-  padding-top: 15px;
-  font-size: 20px
-}
-
-input {
-  width: 80%;
-  padding: 12px 20px;
-  margin: 8px 0;
-  box-sizing: border-box;
-  }
-
-button {
-  background-color: #1655c2;
-  border: none;
-  color: rgb(243, 241, 245);
-  padding: 15px 32px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-  margin: 4px 2px;
-  cursor: pointer;
-}
-
-</style>
