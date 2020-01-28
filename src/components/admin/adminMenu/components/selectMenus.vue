3f524f23c3d99aabdf878242c606584cc53de890
shrestharohit <rohitshr98@gmail.com>
2020-01-28 09:32:37 +0545

    axios header path

--- src/components/admin/adminMenu/components/selectMenus.vue
+++ src/components/admin/adminMenu/components/selectMenus.vue
@@ -53,7 +53,7 @@
 </template>
 
 <script>
-import { instance } from '/home/prabin/Desktop/Canteen/src/store/axiosheader.js'
+import { instance } from '/home/rohitshrestha/git/Canteen-Management/src/store/axiosheader.js'
 export default {
   name: 'menuList',
   data () {
