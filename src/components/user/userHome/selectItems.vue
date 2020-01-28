0153b7513ff82e19aa451e8433fa05a88304396b
mprabs <acharyaprabin03@gmail.com>
2020-01-28 11:38:19 +0545

    reEdit items page

--- src/components/user/userHome/selectItems.vue
+++ src/components/user/userHome/selectItems.vue
@@ -1,52 +1,52 @@
 <template>
 <div id="menuList" >
  <v-content>
-      <v-container class="fill-height" fluid>
-        <v-row align="center" justify="center">
-          <v-col cols="12" sm="8" md="9">
-            <v-card class="elevation-12" height="auto">
-                      <v-toolbar flat >
-                        <v-toolbar-title>Items for the date of {{ dateToday }}</v-toolbar-title>
-                      </v-toolbar>
-                      <hr color="blue" >
-                      <v-card-text >
-                            <v-simple-table fixed-header height="350px">
-                                <template>
-                                  <thead>
-                                    <tr>
-                                      <th>Id</th>
-                                    <th>Name of items</th>
-                                    </tr>
-                                  </thead>
-                                  <tbody>
-                                    <tr v-for="item in todaysmenu" :key="item.id" @click="selectElement(item)" style="cursor: pointer">
-                                      <td> {{ item.id }} </td>
-                                      <td> {{ item.name }} </td>
-                                  </tr>
-                                  </tbody>
-                                </template>
-                              </v-simple-table>
-                     </v-card-text>
-                  </v-card>
-          </v-col>
-          <v-col cols="12" sm="8" md="3">
-                  <v-card class="elevation-12" height="450px">
-                      <v-toolbar flat >
-                        <v-toolbar-title>Your Ordered Item</v-toolbar-title>
-                      </v-toolbar>
-                      <hr color="blue" >
-                      <v-card-text >
-                        <div v-for="thing in selectedItem" :key="thing.id">
-                        <pre slot="text-align:center"><h1> {{ thing.name }} </h1> </pre>
-                        <v-btn block depressed @click="addModal(thing)"> Verify </v-btn>
-                        </div>
-                        <h3 style="color:red"> {{ msg }} </h3>
-                     </v-card-text>
-                  </v-card>
-          </v-col>
-          </v-row>
-      </v-container>
-    </v-content>
+    <v-container class="fill-height" fluid>
+      <v-row align="center" justify="center">
+        <v-col cols="12" sm="8" md="9">
+          <v-card class="elevation-12" height="auto">
+            <v-toolbar flat >
+              <v-toolbar-title>Items for the date of {{ dateToday }}</v-toolbar-title>
+            </v-toolbar>
+            <hr color="blue" >
+            <v-card-text >
+              <v-simple-table fixed-header height="350px">
+                <template>
+                  <thead>
+                    <tr>
+                      <th>Id</th>
+                    <th>Name of items</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr v-for="item in todaysmenu" :key="item.id" @click="selectElement(item)" style="cursor: pointer">
+                      <td> {{ item.id }} </td>
+                      <td> {{ item.name }} </td>
+                    </tr>
+                  </tbody>
+                </template>
+              </v-simple-table>
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col cols="12" sm="8" md="3">
+          <v-card class="elevation-12" height="450px">
+              <v-toolbar flat >
+                <v-toolbar-title>Your Ordered Item</v-toolbar-title>
+              </v-toolbar>
+              <hr color="blue" >
+              <v-card-text >
+                <div v-for="thing in selectedItem" :key="thing.id">
+                <pre slot="text-align:center"><h1> {{ thing.name }} </h1> </pre>
+                <v-btn block depressed @click="addModal(thing)"> Verify </v-btn>
+                </div>
+                <h3 style="color:red"> {{ msg }} </h3>
+              </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-content>
 </div>
 </template>
 
@@ -66,13 +66,12 @@ export default {
   methods: {
     selectElement: function (name) {
       this.selectedItem.unshift(name)
-      // this.selectedItem.splice(1, 1)
+      this.selectedItem.splice(1, 1)
     },
     addModal: function (name) {
       if (this.verifiedStatus === false) {
         alert('Your selected item is ' + name.name)
         var userID = localStorage.getItem('userCredentials')
-        // console.log('username', userID, 'item', name.id)
         var foodId = name.id
         var dateEntry = this.dateToday
         this.$store.dispatch('setOrder', {
@@ -85,15 +84,17 @@ export default {
         this.msg = 'You have already added !'
       }
     },
+    // checkOrder () {
+    //   this.order.forEach(element => {
+    //     if(element.id === )
+    //   });
+    // },
     checkDate () {
       this.dateToday = new Date().toJSON().slice(0, 10)
-      // console.log(this.dateToday)
       for (var i = 0; i < this.items.length; i++) {
         if (this.items[i].date === this.dateToday) {
-          // console.log('yes', this.items[i].food_item.length)
           for (var j = 0; j <= this.items[i].food_item.length; j++) {
             this.foodItems.forEach(element => {
-              // console.log(element)
               if (this.items[i].food_item[j] === element.id) {
                 this.todaysmenu.push(element)
               }
@@ -109,12 +110,17 @@ export default {
     },
     foodItems: function () {
       return this.$store.getters.items
+    },
+    order: function () {
+      return this.$store.getters.order
     }
   },
   mounted () {
     this.$store.dispatch('loadItems')
     this.$store.dispatch('loadSelectedItems')
     this.checkDate()
+    this.$nextTick()
+    console.log(this.items)
     console.log('todaysmenu', this.todaysmenu)
   }
 }
