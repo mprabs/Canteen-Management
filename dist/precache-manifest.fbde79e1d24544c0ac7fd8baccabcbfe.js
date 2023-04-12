d00d9a541a33ad1a76369078cc33a53f44e08dbe
Prabin <acharyaprabin03@gmail.com>
2023-04-12 09:34:34 +0545

    minor

---
+++ dist/precache-manifest.fbde79e1d24544c0ac7fd8baccabcbfe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1fc410bbd0a4cfef3ae",
+    "url": "/Canteen-Management/css/chunk-vendors.0b469407.css"
+  },
+  {
+    "revision": "4f9a2d9f2f4251e2a408241ca877770b",
+    "url": "/Canteen-Management/img/canteen.4f9a2d9f.jpg"
+  },
+  {
+    "revision": "3bce0d3dcff003659eff3a46c77ca8f8",
+    "url": "/Canteen-Management/index.html"
+  },
+  {
+    "revision": "3f20d8e0577909eb393b",
+    "url": "/Canteen-Management/js/app.67757945.js"
+  },
+  {
+    "revision": "f1fc410bbd0a4cfef3ae",
+    "url": "/Canteen-Management/js/chunk-vendors.513d6495.js"
+  },
+  {
+    "revision": "011d1fcc0109cee777ad929d16478851",
+    "url": "/Canteen-Management/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/Canteen-Management/robots.txt"
+  }
+]);
